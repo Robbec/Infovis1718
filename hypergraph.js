6419e37f011eca6be91aa8dbff9dab129c60182f
Unknown <robbe.cools@hotmail.com>
2018-04-19 21:00:06 +0200

    Added combined optin nodes

--- hypergraph.js
+++ hypergraph.js
@@ -9,11 +9,11 @@ var height = 500;
 var xo = 250;
 var yo = 250;
 hypergraph.attr("width", width)
-.attr("height", height);
+  .attr("height", height);
 
 // tooltip aanmaken (inhoud wordt ingevuld bij hover over bolletje)
 var tooltip = hypergraphContainer.append("div")
-.classed("tooltip", true);
+  .classed("tooltip", true);
 
 // voorlopig enkel de 6 opties (gebruikt voor indexering)
 // var options = ["Artificiele intelligentie", "Computationele informatica", "Gedistribueerde systemen", "Mens-machine communicatie", "Software engineering", "Veilige software", "Verdere optie", "Masterproef", "AVO"];
@@ -23,17 +23,17 @@ var tooltip = hypergraphContainer.append("div")
 // var optionColors = {};
 // options.forEach((key, idx) => optionColors[key] = colors[idx]);
 
-d3.csv("cw-2.csv").then(function (data) {  
+d3.csv("cw-2.csv").then(function (data) {
   var columnNames = Object.keys(d3.values(data)[0]);
   var options = columnNames.slice(8, columnNames.length - 1);
-  
+
   // kleurenpalet aan opties koppelen
   // Ziet er niet uit, maar Category10 heeft er 1 te weinig en v5 ondersteunt Category20 niet,
   //  later eigen kleurenschema maken
   var colors = d3.schemePaired;
   var optionColors = {};
   options.forEach((key, idx) => optionColors[key] = colors[idx]);
-  
+
   function getFillColor(d) {
 
   }
@@ -63,183 +63,209 @@ d3.csv("cw-2.csv").then(function (data) {
     return color;
   }
 
-  links = [];
+  var links = [];
+  var comboNodes = [];
 
-    // create links based on options
-    data.forEach(d => {
-        options.forEach(o => {
-            if (d[o] == 1 || d[o] == 2) {
-                links.push({
-                    source: data.length + options.indexOf(o),
-                    target: data.indexOf(d)
-                });
-            }
-        })
-    });
-
-    // nodes zijn data en opties
-    options.forEach(o => data.push({ ID: o, OPO: o }));
-  
-  // bind de cirkels in de hypergraph aan de data
-  var course = hypergraph.selectAll("circle")
-  .data(data);
-
-      // force simulation bepaalt positie
-      var simulation = d3.forceSimulation(data)
-      .force("charge", d3.forceManyBody())
-      .force("collide",d3.forceCollide(11))
-      .force("link", d3.forceLink(links).distance(20).strength(1))
-      .force("x", d3.forceX())
-      .force("y", d3.forceY())
-      .on("tick", refresh);
-  
-  // for (var j = 0; j < options.length; j++) {
-    // var optionGroup = hypergraph.append("g")
-    // .classed("optionGroup", true);
-    // var option = optionGroup.append("circle")
-    // .classed("option", true);
-    // var coursesGroup = optionGroup.append("g");
-    // var courses = data.filter(function (d, i) {
-      // return d[options[j]] == 1;
-    // });
-    // for (var k = 0; k < courses.length; k++) {
-      // coursesGroup.append("circle")
-      // .attr("r", 10);
-    // };
-  // };
-  
-  course.enter()
-  .append("circle")
-  .attr("cx", d => d.x)
-  .attr("cy", d => d.y)
-  .attr("r", 10)
-  .classed("verplicht", function (d) {
-    for (var i = 0; i < options.length; i++) {
-      // TODO Pas dit aan.
-      return d[options[i]] == 1;
+  // create links based on options
+  data.forEach(d => {
+    var ops = [];
+    options.forEach(o => {
+      if (d[o] == 1 || d[o] == 2) {
+        ops.push(o);
+      }
+    })
+    if (ops.length == 1) {
+      // course belongs to only one option
+      links.push({
+        source: data.length + options.indexOf(ops[0]),
+        target: data.indexOf(d)
+      });
+    } else if (ops.length > 1) {
+      // course belongs to more than one option
+      comboName = ops.toString();
+      if (comboNodes.indexOf(comboName) == -1) {
+        comboNodes.push(comboName);
+        ops.forEach(o => {
+          links.push({
+            source: data.length + options.indexOf(o),
+            target: data.length + options.length + comboNodes.indexOf(comboName)
+          });
+        });
+      }
+      links.push({
+        source: data.length + options.length + comboNodes.indexOf(comboName),
+        target: data.indexOf(d)
+      });
     }
-  })
-  .classed("keuze", function (d) {
-    for (var i = 0; i < options.length; i++) {
-      // TODO Pas dit aan.
-      return d[options[i]] == 2;
-    }
-  })
-  .attr("fill", function (d) {
-    return colorOfCourse(d);
-  })  
-  .attr("stroke", function (d) {
-    return colorOfCourse(d);
-  })   
-  .on("mouseover", function (d) {
-    // toon een tooltip voor het gehoverde vak
-    tooltip.classed("active", true)
-    .text(d.OPO)
-    .style("left", (d.x + xo + 20) + "px")
-    .style("top", (d.y + yo - 12) + "px");
-  })
-  .on("mouseout", function (d) {
-    // verberg de tooltip voor het vak waarover gehoverd werd
-    tooltip.classed("active", false);
-  })
-  .on("click", function(d) {
-    var thisCourse = d3.select(this);
-    
-    // verklein de straal van het actieve vak
-    var activeCourse = d3.select("circle.active");
-    activeCourse.attr("r", function () {
-      return activeCourse.attr("r") / 1.75;
-    });
-    
-    // zet het actieve vak op non-actief
-    d3.selectAll("circle").classed("active", false);
-    
-    // verwijder alle inhoud in de infobox
-    infobox.select("p").remove();
-    infobox.select("h3").remove();
-    infobox.select(".points").remove();
-    infobox.select(".checkbox-interested").remove();
-    infobox.select(".checkbox-chosen-master1").remove();
-    infobox.select(".checkbox-chosen-master2").remove();
-    
-    // activeer het geselecteerde vak
-    thisCourse.classed("active", true)
-    .attr("r", function () {
-      return thisCourse.attr("r") * 1.75;
-    });
-    
-    // geef de klasse .prerequisite alleen aan de prerequisites van het actieve vak
-    d3.selectAll("circle")
-    .classed("prerequisite", function (dcircle) {
-      var id = dcircle.ID;
-      return d["Gelijktijdig volgen"].split(" ").includes(id);
-    });
-    
-    // maak nieuwe inhoud aan in de infobox:
-    // 1) titel van het actieve vak
-    infobox.append("h3").text(d.OPO);
-    
-    // 2) studiepunten van het actieve vak
-    infobox.append("div")
-    .attr("class", "points")
-    .text(d.Studiepunten + " SP");
-    
-    // 3) checkbox "Niet geïnteresseerd" voor het actieve vak
-    var checkboxInterested = infobox.append("label")
-    .text("Niet geïnteresseerd in dit vak.");
-    checkboxInterested.attr("class","checkbox checkbox-interested")
-    .append("input")
-    .attr("type", "checkbox")
-    .property("checked", thisCourse.classed("not-interested"))
-    .property("checked", thisCourse.classed("is-not-interested"));
-    checkboxInterested.append("span")
-    .attr("class", "checkmark");
-    
-    // 4) checkbox "Kies in 1ste Master" voor het actieve vak
-    var checkboxChoose1 = infobox.append("label")
-    .text("Kies dit vak in 1ste Master.");
-    checkboxChoose1.attr("class","checkbox checkbox-chosen-master1")
-    .append("input")
-    .attr("type", "checkbox")
-    .property("checked", thisCourse.classed("chosen-master1"));
-    checkboxChoose1.append("span")
-    .attr("class", "checkmark");
-    
-    // 5) checkbox "Kies in 2de Master" voor het actieve vak
-    var checkboxChoose2 = infobox.append("label")
-    .text("Kies dit vak in 2de Master.");
-    checkboxChoose2.attr("class","checkbox checkbox-chosen-master2")
-    .append("input")
-    .attr("type", "checkbox")
-    .property("checked", thisCourse.classed("chosen-master2"));
-    checkboxChoose2.append("span")
-    .attr("class", "checkmark");
   });
 
-  var lines = hypergraph.selectAll("line")
-            .data(links);
+  // nodes zijn data en opties
+  
+  var extraNodes = [];
+  options.forEach(o => extraNodes.push({ ID: o, OPO: o }));
+  comboNodes.forEach(n => extraNodes.push({ID: n, OPO: n}));
+  var nodes = [...data, ...extraNodes];
 
-        lines.enter()
-            .append("line")
-            .attr("x1", l => l.source.x + xo)
-            .attr("y1", l => l.source.y + yo)
-            .attr("x2", l => l.target.x + xo)
-            .attr("y2", l => l.target.y + yo)
-            .classed("link",true);
+  // force simulation bepaalt positie
+  var simulation = d3.forceSimulation(nodes)
+    .force("charge", d3.forceManyBody())
+    .force("collide", d3.forceCollide(11))
+    .force("link", d3.forceLink(links).distance(15).strength(2))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
+    .on("tick", refresh);
+
+  // for (var j = 0; j < options.length; j++) {
+  // var optionGroup = hypergraph.append("g")
+  // .classed("optionGroup", true);
+  // var option = optionGroup.append("circle")
+  // .classed("option", true);
+  // var coursesGroup = optionGroup.append("g");
+  // var courses = data.filter(function (d, i) {
+  // return d[options[j]] == 1;
+  // });
+  // for (var k = 0; k < courses.length; k++) {
+  // coursesGroup.append("circle")
+  // .attr("r", 10);
+  // };
+  // };
+
+  // bind de cirkels in de hypergraph aan de data
+  var course = hypergraph.selectAll("circle")
+    .data(nodes);
+
+  course.enter()
+    .append("circle")
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y)
+    .attr("r", 10)
+    .classed("verplicht", function (d) {
+      for (var i = 0; i < options.length; i++) {
+        // TODO Pas dit aan.
+        return d[options[i]] == 1;
+      }
+    })
+    .classed("keuze", function (d) {
+      for (var i = 0; i < options.length; i++) {
+        // TODO Pas dit aan.
+        return d[options[i]] == 2;
+      }
+    })
+    .attr("fill", function (d) {
+      return colorOfCourse(d);
+    })
+    .attr("stroke", function (d) {
+      return colorOfCourse(d);
+    })
+    .on("mouseover", function (d) {
+      // toon een tooltip voor het gehoverde vak
+      tooltip.classed("active", true)
+        .text(d.OPO)
+        .style("left", (d.x + xo + 20) + "px")
+        .style("top", (d.y + yo - 12) + "px");
+    })
+    .on("mouseout", function (d) {
+      // verberg de tooltip voor het vak waarover gehoverd werd
+      tooltip.classed("active", false);
+    })
+    .on("click", function (d) {
+      var thisCourse = d3.select(this);
+
+      // verklein de straal van het actieve vak
+      var activeCourse = d3.select("circle.active");
+      activeCourse.attr("r", function () {
+        return activeCourse.attr("r") / 1.75;
+      });
+
+      // zet het actieve vak op non-actief
+      d3.selectAll("circle").classed("active", false);
+
+      // verwijder alle inhoud in de infobox
+      infobox.select("p").remove();
+      infobox.select("h3").remove();
+      infobox.select(".points").remove();
+      infobox.select(".checkbox-interested").remove();
+      infobox.select(".checkbox-chosen-master1").remove();
+      infobox.select(".checkbox-chosen-master2").remove();
+
+      // activeer het geselecteerde vak
+      thisCourse.classed("active", true)
+        .attr("r", function () {
+          return thisCourse.attr("r") * 1.75;
+        });
+
+      // geef de klasse .prerequisite alleen aan de prerequisites van het actieve vak
+      d3.selectAll("circle")
+        .classed("prerequisite", function (dcircle) {
+          var id = dcircle.ID;
+          return d["Gelijktijdig volgen"].split(" ").includes(id);
+        });
+
+      // maak nieuwe inhoud aan in de infobox:
+      // 1) titel van het actieve vak
+      infobox.append("h3").text(d.OPO);
+
+      // 2) studiepunten van het actieve vak
+      infobox.append("div")
+        .attr("class", "points")
+        .text(d.Studiepunten + " SP");
+
+      // 3) checkbox "Niet geïnteresseerd" voor het actieve vak
+      var checkboxInterested = infobox.append("label")
+        .text("Niet geïnteresseerd in dit vak.");
+      checkboxInterested.attr("class", "checkbox checkbox-interested")
+        .append("input")
+        .attr("type", "checkbox")
+        .property("checked", thisCourse.classed("not-interested"))
+        .property("checked", thisCourse.classed("is-not-interested"));
+      checkboxInterested.append("span")
+        .attr("class", "checkmark");
+
+      // 4) checkbox "Kies in 1ste Master" voor het actieve vak
+      var checkboxChoose1 = infobox.append("label")
+        .text("Kies dit vak in 1ste Master.");
+      checkboxChoose1.attr("class", "checkbox checkbox-chosen-master1")
+        .append("input")
+        .attr("type", "checkbox")
+        .property("checked", thisCourse.classed("chosen-master1"));
+      checkboxChoose1.append("span")
+        .attr("class", "checkmark");
+
+      // 5) checkbox "Kies in 2de Master" voor het actieve vak
+      var checkboxChoose2 = infobox.append("label")
+        .text("Kies dit vak in 2de Master.");
+      checkboxChoose2.attr("class", "checkbox checkbox-chosen-master2")
+        .append("input")
+        .attr("type", "checkbox")
+        .property("checked", thisCourse.classed("chosen-master2"));
+      checkboxChoose2.append("span")
+        .attr("class", "checkmark");
+    });
+
+  var lines = hypergraph.selectAll("line")
+    .data(links);
+
+  lines.enter()
+    .append("line")
+    .attr("x1", l => l.source.x + xo)
+    .attr("y1", l => l.source.y + yo)
+    .attr("x2", l => l.target.x + xo)
+    .attr("y2", l => l.target.y + yo)
+    .classed("link", true);
 
   function refresh() {
     hypergraph.selectAll("circle")
-        .data(data)
-        .attr("cx", d => d.x + xo)
-        .attr("cy", d => d.y + yo);
+      .data(nodes)
+      .attr("cx", d => d.x + xo)
+      .attr("cy", d => d.y + yo);
 
     hypergraph.selectAll("line")
-        .data(links)
-        .attr("x1", l => l.source.x + xo)
-        .attr("y1", l => l.source.y + yo)
-        .attr("x2", l => l.target.x + xo)
-        .attr("y2", l => l.target.y + yo);
-}
+      .data(links)
+      .attr("x1", l => l.source.x + xo)
+      .attr("y1", l => l.source.y + yo)
+      .attr("x2", l => l.target.x + xo)
+      .attr("y2", l => l.target.y + yo);
+  }
 
 });
 
@@ -258,14 +284,14 @@ infobox.on("change", function () {
     checkboxInterested = checkboxInterestedNew;
     checkboxInterestedChanged();
   }
-  
+
   // controleer of de checkbox "Kies in 1ste Master" van status verandert
   var checkboxChosenMaster1New = infobox.select(".checkbox-chosen-master1").select("input");
   if (checkboxChosenMaster1 !== checkboxChosenMaster1New) {
     checkboxChosenMaster1 = checkboxChosenMaster1New;
     checkboxChosenMaster1Changed();
   }
-  
+
   // controleer of de checkbox "Kies in 2de Master" van status verandert
   var checkboxChosenMaster2New = infobox.select(".checkbox-chosen-master2").select("input");
   if (checkboxChosenMaster2 !== checkboxChosenMaster2New) {
@@ -279,7 +305,7 @@ function checkboxInterestedChanged() {
   var switchInterestedChecked = switchInterested.property("checked");
   var activeCourse = d3.select("circle.active");
   var checked = checkboxInterested.property("checked");
-  
+
   // voeg de klasse .is-not-interested toe als een vak gemarkeerd is als "Niet geïnteresseerd" en getoond moet worden in de hypergraph
   if (checked && switchInterestedChecked) {
     activeCourse.classed("is-not-interested", true);
@@ -291,7 +317,7 @@ function checkboxInterestedChanged() {
   // verwijder de klassen .not-interested en .is-not-interested als een vak niet gemarkeerd is als "Niet geïnteresseerd"
   else {
     activeCourse.classed("not-interested", false)
-    .classed("is-not-interested", false);
+      .classed("is-not-interested", false);
   }
 };
 
@@ -300,18 +326,18 @@ switchInterested.on("change", function () {
   // wijzig de klassen .not-interested naar .is-not-interested als de switch wordt ingeschakeld
   if (switchInterested.property("checked")) {
     d3.selectAll("circle")
-    .classed("is-not-interested", function () {
-      return d3.select(this).classed("not-interested");
-    })
-    .classed("not-interested", false);
+      .classed("is-not-interested", function () {
+        return d3.select(this).classed("not-interested");
+      })
+      .classed("not-interested", false);
   }
   // wijzig de klassen .is-not-interested naar .not-interested als de switch wordt uitgeschakeld
   else {
     d3.selectAll("circle")
-    .classed("not-interested", function () {
-      return d3.select(this).classed("is-not-interested");
-    })
-    .classed("is-not-interested", false);
+      .classed("not-interested", function () {
+        return d3.select(this).classed("is-not-interested");
+      })
+      .classed("is-not-interested", false);
   }
 });
 
@@ -319,7 +345,7 @@ switchInterested.on("change", function () {
 function checkboxChosenMaster1Changed() {
   var activeCourse = d3.select("circle.active");
   var checked = checkboxChosenMaster1.property("checked");
-  
+
   // voeg de klasse .chosen-master1 toe aan het actieve vak als het gemarkeerd is als "Gekozen in 1ste Master"
   activeCourse.classed("chosen-master1", function () {
     return checked;
@@ -330,7 +356,7 @@ function checkboxChosenMaster1Changed() {
 function checkboxChosenMaster2Changed() {
   var activeCourse = d3.select("circle.active");
   var checked = checkboxChosenMaster2.property("checked");
-  
+
   // voeg de klasse .chosen-master2 toe aan het actieve vak als het gemarkeerd is als "Gekozen in 2de Master"
   activeCourse.classed("chosen-master2", function () {
     return checked;
