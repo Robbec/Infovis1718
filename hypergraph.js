57a34c19943fbb5e34c59e05edc8255e5c212e85
Niels Van Belle <niels.vanbelle@hotmail.com>
2018-05-09 08:49:27 +0200

    versies van vakken tijdelijk negeren om juiste structuur te krijgen

--- hypergraph.js
+++ hypergraph.js
@@ -37,7 +37,7 @@ hypergraph.attr("width", svgWidth)
 var tooltip = hypergraphContainer.append("div")
   .classed("tooltip", true);
 
-d3.csv("cw-5.csv").then(function(data) {
+d3.csv("cw-6-tijdelijk.csv").then(function(data) {
   d3.csv("uniekeReserveringen.csv").then(function(scheduleData) {
     // namen van alle opties
     var columnNames = d3.keys(d3.values(data)[0]);
@@ -51,7 +51,7 @@ d3.csv("cw-5.csv").then(function(data) {
     //var colors = ['rgb(77, 203, 77)', 'rgb(130, 77, 203)', 'rgb(203, 138, 77)', 'rgb(203, 203, 77)', 'rgb(77, 132, 203)', 'rgb(203, 77, 144)'];
     // var colors = ['rgb(203, 140, 77)', 'rgb(77, 203, 77)', 'rgb(203, 203, 77)', 'rgb(77, 203, 203)', 'rgb(77, 77, 203)', 'rgb(161, 77, 203)', 'rgb(203, 77, 203)', 'rgb(203, 77, 77)'];
     // var colors = ['hsl(300, 100%, 50%)', 'hsl(30, 100%, 50%)', 'hsl(60, 100%, 50%)', 'hsl(120, 100%, 50%)', 'hsl(180, 100%, 50%)', 'hsl(240, 100%, 50%)', 'hsl(266, 100%, 50%)'];
-    var colors = [30, 60, 120, 180, 240, 266, 300];
+    var colors = [30, 60, 180, 120, 240, 300]; // 266 magenta
     var optionColors = [];
     optionNames.forEach((c, i) => optionColors[c] = colors[i]);
     var kulBlue = "#1d8db0";
